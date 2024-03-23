1ca487871b20cd2d555281a0132e8060c4c49dc3
Eni7 <karolenahor@gmail.com>
2024-03-23 17:48:18 +0100

    cokolwiek

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const YourComponent = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [toDoItems, setToDoItems] = useState([]);
+
+  const handleButtonClick = () => {
+    const newItem = {
+      id: Math.random().toString(36).substr(2, 9),
+      text: inputValue
+    };
+
+    setToDoItems(prevItems => [...prevItems, newItem]);
+    setInputValue('');
+  };
+
+  const handleDelete = id => {
+    setToDoItems(prevItems => prevItems.filter(item => item.id !== id));
+  };
+
+  const handleEdit = id => {
+    const newText = prompt("Enter new text:");
+    if (newText) {
+      setToDoItems(prevItems =>
+        prevItems.map(item => (item.id === id ? { ...item, text: newText } : item))
+      );
+    }
+  };
+
+  return (
+    <div>
+      <input
+        id="check"
+        type="text"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
+      <button id="big" onClick={handleButtonClick}>
+        Add Item
+      </button>
+      <div id="list">
+        {toDoItems.map(item => (
+          <div key={item.id} style={{ display: "flex", justifyContent: "left", flexDirection: "row-reverse" }}>
+            <div>{item.text}</div>
+            <div>
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+              <button onClick={() => handleEdit(item.id)}>Edit</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default YourComponent;
---
+++ src/cointainers/Essa.js
@@ -0,0 +1,38 @@
+import React from "react"
+const Essas = () => {
+
+    return (
+        
+        <div>
+        <button>Click</button>
+        <button>hello</button>
+    
+     
+        </div>
+    )
+}
+function MyButton() {
+    return (
+      <button>
+        Click here!!
+      </button>
+    );
+  }
+  
+  export default function Essa() {
+    return (
+      <div>
+        
+        <h1>Welcome on my page</h1>
+        <MyButton />
+        <p>Jak sie masz. <br /> </p>
+        <ul><li>
+            dasda
+        </li>
+        <li></li>
+        </ul>
+      </div>
+    );
+  }
+
+// export default Essa
